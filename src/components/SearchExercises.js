3ce239ca39ef272134c05070e59fcda9fadba52c
Ricky Wu <rickywu209@gmail.com>
2023-01-13 23:11:19 -0700

    setting up our exercise api to fetch data

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -5,6 +5,12 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 const SearchExercises = () => {
     const [search, setSearch] = useState('')
 
+    const handleSearch = async () => {
+        if(search) { //if search exists, fetch data
+            const exercisesData = await fetchData();
+        }
+    }
+
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
         <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '30px'}}} mb="50px" textAlign="center">
