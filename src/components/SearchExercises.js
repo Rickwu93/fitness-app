bb19ab7d430bd390c5640ce37a400e8ff82f7cc3
Ricky Wu <rickywu209@gmail.com>
2023-01-16 22:56:11 -0700

    adding a scrollbar with each bodypart

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -6,9 +6,8 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 
 import HorizontalScrollbar from './HorizontalScrollbar';
 
-const SearchExercises = () => {
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
     const [search, setSearch] = useState('')
-    const [exercises, setExercises] = useState([]);
     const [bodyParts, setBodyParts] = useState([])
 
     useEffect(() => {
@@ -79,7 +78,7 @@ const SearchExercises = () => {
         </Button>
         </Box>
         <Box sx={{ position: 'relative', width: '100%', p: '20px'}}>
-            <HorizontalScrollbar  data={bodyParts} />
+            <HorizontalScrollbar  data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} />
         </Box>
     </Stack>
   )
