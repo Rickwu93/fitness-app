8447d2600991725fa0aa7528bb2ee2e081de5244
Ricky Wu <rickywu209@gmail.com>
2023-01-22 22:11:00 -0700

    adding basic page pagination behaviour when loading

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,6 +6,15 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const exercisePerPage = 9;
+
+  const paginate = (e, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: 'smooth'})
+  }
   
   return (
     <Box id="exercises"
@@ -19,6 +28,19 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         <Stack direction="row" sx={{ gap: { lg: '110px', xs: '50px'}}} flexWrap="wrap" justifyContent="center">
             {exercises.map((exercise, index) => 
               <ExerciseCard key={index} exercise={exercise} />
+            )}
+        </Stack>
+        <Stack mt="100px" alignItems="center">
+            {exercises.length > 9 && (
+              <Pagination
+                color="standard"
+                shape="rounded"
+                defaultPage={1}
+                count={Math.ceil(exercises.length / exercisePerPage)}
+                page={currentPage}
+                onChange={paginate}
+                size="large"
+              />
             )}
         </Stack>
     </Box>
