f6460e4b3bc530cc8cb6001afc2d47117050d4eb
Ricky Wu <rickywu209@gmail.com>
2023-01-23 22:19:35 -0700

    fixexerciseperpage not showing up

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Pagination } from '@mui/material/Pagination';
+import Pagination from '@mui/material/Pagination';
 import { Box, Stack, Typography } from '@mui/material/';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
@@ -8,8 +8,9 @@ import ExerciseCard from './ExerciseCard';
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
 
   const [currentPage, setCurrentPage] = useState(1);
-  const exercisePerPage = 9;
+  const exercisesPerPage = 9;
 
+  //pagination
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
   const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
@@ -40,7 +41,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
                 color="standard"
                 shape="rounded"
                 defaultPage={1}
-                count={Math.ceil(exercises.length / exercisePerPage)}
+                count={Math.ceil(exercises.length / exercisesPerPage)}
                 page={currentPage}
                 onChange={paginate}
                 size="large"
