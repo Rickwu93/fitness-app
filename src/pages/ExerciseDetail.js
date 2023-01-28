9fc781aebf6d03d106d476c1b3313c9c432f31e5
Ricky Wu <rickywu209@gmail.com>
2023-01-27 19:35:13 -0700

    sending exerciseVideos data to the ExerciseVideo component

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,16 +2,18 @@ import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material'
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const { id } = useParams(); //gives us the url id number for each exercise
 
   //recalling function whenever id changes
+  //exercise and youtube api call
   useEffect(() => {
     const fetchExercisesData = async () => {
       const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
@@ -20,6 +22,10 @@ const ExerciseDetail = () => {
       const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`,
       exerciseOptions); //exerciseOption is the key that allows us to make the request
       setExerciseDetail(exerciseDetailData);
+      //gives video link only for specific 
+      const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?q=$
+      {exerciseDetailData.name}`, youtubeOptions);
+      setExerciseVideos(exerciseVideosData)
 
     }
 
@@ -29,7 +35,7 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos exerciseVideos={exerciseVideos} />
       <SimilarExercises />
     </Box>
   )
