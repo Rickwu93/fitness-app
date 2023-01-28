73b16dfba1b88222782dc7b47e42127ef13b82fc
Ricky Wu <rickywu209@gmail.com>
2023-01-27 18:18:22 -0700

    adding in youtube fetchData for the host/key

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -39,8 +39,8 @@ const Detail = ({ exerciseDetail }) => {
             {extraDetail.map((item) => (
                 <Stack key={item.name} direction="row" gap="24px" alignItems="center">
                     <Button sx={{ background: '#fff2db', borderRadius: '50%', width: '100px', height: '100px'}}>
-                        <img src={item.icon} />
-                        <Typography variant="h5">
+                        <img src={item.icon} alt={bodyPart} style={{ width: '50px', height: '50px' }}/>
+                        <Typography textTransform="capitalize" variant="h5">
                             {item.name}
                         </Typography>
                     </Button>
