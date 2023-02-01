2c44d90cdc58978f31431180a28fd61ccfffe71b
Ricky Wu <rickywu209@gmail.com>
2023-02-01 01:15:15 -0700

    added additional similar exercises that use same equipment

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -7,11 +7,19 @@ import Loader from './Loader';
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: '100px', xs: '0' }}}>
-        <Typography variant="h3">
+        <Typography variant="h3" mb={5} >
             Exercises that target the same muscle group
             <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
                {targetMuscleExercises.length ? 
                 <HorizontalScrollbar data={targetMuscleExercises} />
+                : <Loader /> }
+            </Stack>
+            <Typography variant="h3" mb={5} >
+            Exercises that use the same equipment</Typography>
+            <Stack direction="row" sx={{ p: '2', position: 'relative' }
+        }>
+               {equipmentExercises.length ? 
+                <HorizontalScrollbar data={equipmentExercises} />
                 : <Loader /> }
             </Stack>
         </Typography>
