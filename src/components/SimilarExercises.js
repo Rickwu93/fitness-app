9b58c87facf291b0957f48199354c13d68914f43
Ricky Wu <rickywu209@gmail.com>
2023-02-01 00:53:01 -0700

    added conditional rendering for similar exercise card

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Box, Stack, Typography } from '@mui/material';
 
 import HorizontalScrollbar from './HorizontalScrollbar';
-import Loader from './Loader'
+import Loader from './Loader';
 
 const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
@@ -10,7 +10,9 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
         <Typography variant="h3">
             Exercises that target the same muscle group
             <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
-                {targetMuscleExercises.length && <HorizontalScrollbar data={targetMuscleExercises} />}
+               {targetMuscleExercises.length ? 
+                <HorizontalScrollbar data={targetMuscleExercises} />
+                : <Loader /> }
             </Stack>
         </Typography>
     </Box>
